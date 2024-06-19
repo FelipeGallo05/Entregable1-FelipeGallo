1fb13e4f3a028c8d9e0d00e6fbf3334f885048b0
Felipe Gallo <gallofelipe6@gmail.com>
2024-06-19 16:53:55 -0300

    Agregue funciones en cada una de las figuras

--- main.js
+++ main.js
@@ -7,29 +7,42 @@ switch (figura){
         let b = Number(prompt('Cual es el valor de la base?'));
         let h = Number(prompt('Cual es el valor de la altura?'));
         alert('Calculando...');
-        areaTri1 = b * h;
-        areaTri2 = areaTri1 / 2;
+        function triangulo(b,h){
+            areaTri1 = b * h;
+            areaTri2 = areaTri1 / 2
+            return areaTri2;
+        }
+        triangulo(b, h)
         alert('El area es = '+ areaTri2 + ' (unidad que tenga, sea cm m dm dam km)^2')
         break;
 
     case 'cuadrado':
         let l = Number(prompt('Cual es el valor de cualquiera de los lados?'));
-        areaCuad = l * l;
+        function cuadrado(l){
+            areaCuad = l * l
+        }
         alert('Calculando...');
+        cuadrado(l)
         alert('El area es = '+ areaCuad + ' (unidad que tenga, sea cm m dm dam km)^2');
         break;
     
     case 'rectangulo':
         let baseRect = Number(prompt('Cual es el valor del ancho de la figura?'));
         let a = Number(prompt('Cual es el valor del largo de la figura?'))
-        areaRect = baseRect * a;
+        function rectangulo(baseRect, a){
+            areaRect = baseRect * a;
+        }
+        rectangulo(baseRect, a)
         alert('Calculando...');
         alert('El area es = '+ areaRect + ' (unidad que tenga, sea cm m dm dam km)^2');
         break;
 
     case 'circulo':
         let r = Number(prompt('Cual es el valor del radio?'))
-        areaCirc = pi * r * r
+        function circulo(r){
+            areaCirc = pi * r * r;
+        }
+        circulo(r)
         alert('Calculando...');
         alert('El area es = '+ areaCirc + ' (unidad que tenga, sea cm m dm dam km)^2');
         break;
@@ -37,7 +50,10 @@ switch (figura){
     case 'paralelogramo':
         let basePar = Number(prompt('Cual es el valor de la base?'))
         let hPar = Number(prompt('Cual es el valor de la altura?'))
-        areaPar = basePar * hPar
+        function paralelogramo(basePar, hPar){
+            areaPar= basePar * hPar
+        }
+        paralelogramo(basePar, hPar)
         alert('Calculando...')
         alert('El area es = ' + areaPar + ' (unidad que tenga, sea cm m dm dam km)^2')
 
